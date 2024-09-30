8c654252c8d554afcecb38e66f46af246545c158
ELiton Camargo <camargoliveira@gmail.com>
2024-09-30 10:27:47 -0300

    App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>App Name</Text>
+      <Text>App Name Teste</Text>
       <StatusBar style="auto" />
     </View>
   );
